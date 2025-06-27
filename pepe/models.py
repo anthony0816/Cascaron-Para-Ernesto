65b4ab21d29caa7171fc913aa65ab08c8eba9cc3
anthony0816 <149899054+anthony0816@users.noreply.github.com>
2025-06-26 23:44:21 -0400

    ...

--- pepe/models.py
+++ pepe/models.py
@@ -1,55 +1,61 @@
 from django.db import models
 
-ANIMAL_CHOICES = [
+
+
+
+
+
+class Animal(models.Model):
+    ANIMAL_CHOICES = [
     ("perro","Perro"),
     ("gato","Gato"),
     ("hamster","Hámster"),
     ("ave","Ave"),
 ]
-SEX_CHOICES = [
+
+    SEX_CHOICES = [
     ("M","Macho"),
     ("H", "Hembra"),
-]
-TIPO_CHOICES = [
+    ]
+
+    nombre = models.CharField(max_length=100)
+    especie = models.CharField(max_length=30, choices=ANIMAL_CHOICES)
+    edad = models.PositiveIntegerField()
+    sexo = models.CharField(max_length=2 ,choices=SEX_CHOICES)
+    disp_adopcion = models.BooleanField(default= True)
+
+
+class AtencionMedica(models.Model):
+    
+    TIPO_CHOICES = [
     ("consulta", "Consulta"),
     ("vacunacion","Vacunacion"),
 ]
-VACUNAS_CHOICES = [
-    ("anti-rabia","Anti-Rabia"),
-    ("anti-leucemia_felina","Anti-leucemia_felina"),
-    ("anti-tifoidea","Anti-tifoidea"),
-    ("anti-viruela","Anti-viruela"),
-]
-
-VETERINARIOS_CHOICE = [
+    
+    VETERINARIOS_CHOICE = [
     ("dr_andres_herrera","Dr. Andrés Herrera"),
     ("dra_clara_nuñez","Dra. Clara Núñez"),
     ("dra_sofia_mena","Dra. Sofía Mena"),
     ("dr_luis_torres","Dr. Luis Torres"),
 ]
 
-
-
-class Animal(models.Model):
-    nombre = models.CharField(max_length=100)
-    especie = models.CharField(max_length=30, choices=ANIMAL_CHOICES)
-    edad = models.PositiveIntegerField()
-    sexo = models.CharField(max_length=2 ,choices=SEX_CHOICES)
-    disp_adopcion = models.BooleanField(null=True)
-
-
-class AtencionMedica(models.Model):
     tipo = models.CharField(max_length=20, choices=TIPO_CHOICES)
     animal= models.ForeignKey(Animal, on_delete=models.CASCADE)
     veterinario = models.CharField(max_length=30, choices=VETERINARIOS_CHOICE)
     fecha = models.DateTimeField()
     
 class Consulta(AtencionMedica):
-    atención_medica = models.OneToOneField(AtencionMedica, on_delete=models.CASCADE, parent_link= True)
+    atención_medica = models.OneToOneField(AtencionMedica, on_delete=models.CASCADE)
     diagnostico = models.CharField(max_length=100)
     tratamiento = models.CharField(max_length=100)
 
 
 class Vacunacion(AtencionMedica):
-    atención_medica = models.OneToOneField(AtencionMedica, on_delete=models.CASCADE, parent_link= True)
+    VACUNAS_CHOICES = [
+    ("anti-rabia","Anti-Rabia"),
+    ("anti-leucemia_felina","Anti-leucemia_felina"),
+    ("anti-tifoidea","Anti-tifoidea"),
+    ("anti-viruela","Anti-viruela"),
+    ]
+    atención_medica = models.OneToOneField(AtencionMedica, on_delete=models.CASCADE)
     vacuna = models.CharField(max_length=100, choices=VACUNAS_CHOICES)
--- pepe/views.py
+++ pepe/views.py
@@ -19,82 +19,17 @@ def registrar(request):
         sexo = request.POST.get("sexo"),
         disp_adopcion = request.POST.get("En adopcion")
 
-<<<<<<< HEAD
-
-
-def iniciar_sesion(request):
-    if request.method == 'POST':
-        usuario = request.POST['nombre']
-        clave = request.POST['password']
-        user = authenticate(request, username=usuario, password=clave)
-        if user is not None:
-            login(request, user)
-            return redirect('listar')  
-        else:
-            error = 'Credenciales inválidas'
-            return render(request, 'login.html', {'error': error})
-    return render(request, 'login.html')
-
-def cerrar_sesion(request):
-    logout(request)
-    return redirect("/")
-
-@login_required 
-def main(request):
-    return render(request,"main.html",)
-
-@login_required 
-def listar(request):
-    libros = libro.objects.all()
-    if libro is not None:
-        return render(request,"all_elements.html", {"libros": libros} )
-    return render(request,"all_elements.html")
-
-@login_required 
-def crear(request):
-    if request.method == 'POST':
-        titulo = request.POST.get("titulo")
-        descripcion = request.POST.get("descripcion")
-        precio = request.POST.get("precio")
-        
-        libro.objects.create(
-            titulo = titulo,
-            descripcion = descripcion,
-            precio = precio
-=======
         anm= Animal.objects.create(
             nombre,
             especie,
             edad,
             sexo,
             disp_adopcion,
->>>>>>> 348ed930c90c654e826a042a0beb0438ff344381
         )
         
         return redirect("registrar/")
     return render(request,"crear.html")
 
-<<<<<<< HEAD
-@login_required 
-def editar(request,id):
-    if request.method == 'POST':
-        
-        elemento = libro.objects.get(id=id)
-        elemento.titulo = request.POST.get("titulo")
-        elemento.descripcion = request.POST.get("descripcion")
-        elemento.precio = request.POST.get("precio")
-        elemento.save()
-        
-        return redirect("/listar")
-    elemento = libro.objects.get(id=id)
-    return render(request, "editar.html", {"libro": elemento})
-
-@login_required 
-def eliminar(request,id):
-    elemento = libro.objects.get(id=id)
-    elemento.delete()
-    return redirect("/listar")
-=======
 
 def iniciar(request):
     nombre =  request.POST["username"]
@@ -109,4 +44,3 @@ def iniciar(request):
 def desloguear(request):
     logout(request)
     return render(request, "login/")
->>>>>>> 348ed930c90c654e826a042a0beb0438ff344381
